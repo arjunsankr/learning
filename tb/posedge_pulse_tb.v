393f019368779bcf6fe7092e11bd1d046ba9ccd5
arjunsankr <arjunsankarop@gmail.com>
2026-07-29 23:25:15 +0530

    Rename testbench module from posedge_pulse_tb to rising_edge_detect_tb

--- tb/posedge_pulse_tb.v
+++ tb/posedge_pulse_tb.v
@@ -1,11 +1,11 @@
-module posedge_pulse_tb;
+module rising_edge_detect_tb;
   reg clk;
   reg rstn;
   reg in0;
   wire op0;
   
   
-  posedge_pulse dut(clk,rstn,in0,op0);
+  rising_edge_detect dut(clk,rstn,in0,op0);
   always #5 clk=~clk;
   
   initial begin
