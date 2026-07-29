f73f790a983f565c2a91d7a7e62dffe960484229
arjunsankr <arjunsankarop@gmail.com>
2026-07-29 19:27:03 +0530

    Add posedge_pulse testbench
    This testbench simulates the behavior of the posedge_pulse module, generating clock signals and testing input variations.

---
+++ tb/posedge_pulse_tb.v
@@ -0,0 +1,31 @@
+module posedge_pulse_tb;
+  reg clk;
+  reg rstn;
+  reg in0;
+  wire op0;
+  
+  
+  posedge_pulse dut(clk,rstn,in0,op0);
+  always #5 clk=~clk;
+  
+  initial begin
+    clk=0;
+    repeat(2) @(posedge clk);
+    rstn=1'b0;
+    @(posedge clk);begin
+    in0=1;
+      rstn=1;end
+    repeat(4) @(posedge clk);
+    in0=0;
+    @(posedge clk);
+    in0=1;
+    repeat(2) @(posedge clk);
+    in0=0;
+    @(posedge clk);
+    in0=1;
+    $finish;
+  end
+  
+  initial begin
+    $dumpfile("dumo.vcd");$dumpvars;end
+endmodule
