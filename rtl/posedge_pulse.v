5a9d6caaabb2ff78ae99134f9a78b8c3b5e4ae78
arjunsankr <arjunsankarop@gmail.com>
2026-07-29 19:26:28 +0530

    posedge_pulse module for pulse generation

---
+++ rtl/posedge_pulse.v
@@ -0,0 +1,19 @@
+module posedge_pulse(input 
+                     clk,
+                     input rstn,
+                     input in0,
+                     output reg op0);
+  
+  reg o1;
+  
+  always @(posedge clk)
+    begin
+      if(!rstn)
+        o1<=0;
+      else begin
+        o1<=in0;
+        op0<=in0&~o1;
+      end
+    end  
+endmodule
+        
