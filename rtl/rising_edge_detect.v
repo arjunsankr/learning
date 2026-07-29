a3e581cd8efa262e9d7b23278901e40df05eac96
arjunsankr <arjunsankarop@gmail.com>
2026-07-30 00:11:28 +0530

    Fix rising edge detection logic and output naming

--- rtl/rising_edge_detect.v
+++ rtl/rising_edge_detect.v
@@ -2,17 +2,18 @@ module rising_edge_detect(input
                      clk,
                      input rstn,
                      input in0,
-                     output reg op0);
+                     output reg o_p);
   
-  reg o1;
+  reg in0_1d;
   
   always @(posedge clk)
     begin
-      if(!rstn)
-        o1<=0;
+      if(!rstn)begin
+        in0_1d<=1'b0;
+        o_p<=1'b0;
       else begin
-        o1<=in0;
-        op0<=in0&~o1;
+        in0_1d<=in0;
+        o_p<=in0&~in0_1d;
       end
     end  
 endmodule
