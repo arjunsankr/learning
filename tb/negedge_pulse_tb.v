db9830beae59e81aa5f08af911a880a9ecc3868b
arjunsankr <arjunsankarop@gmail.com>
2026-07-29 23:24:14 +0530

    Rename test module and instance for falling edge detection

--- tb/negedge_pulse_tb.v
+++ tb/negedge_pulse_tb.v
@@ -1,11 +1,11 @@
-module posedge_pulse_tb;
+module falling_edge_detect_tb;
   reg clk;
   reg rstn;
   reg in0;
   wire o_p;
   
   
-  negedge_pulse dut(clk,rstn,in0,op0);
+  flling_edge_detect dut(clk,rstn,in0,op0);
   always #5 clk=~clk;
   
   initial begin
