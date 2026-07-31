b675c86af2ba37c890db1b326af6364f3334816d
arjunsankr <arjunsankarop@gmail.com>
2026-07-31 20:07:49 +0530

    sqdt_1101_moore_ovr.v

---
+++ rtl/sqdt_1101_moore_ovr.v
@@ -0,0 +1,59 @@
+  module sqdt_1101_moore_ovr(input clk,rstn,
+                         input in,
+                         output detect);
+  
+    reg [2:0]n_state,p_state;
+  
+      
+      //current state logic 
+      
+  always@(posedge clk or negedge rstn) begin
+    if(!rstn)
+    p_state<=3'b000;
+    else
+      p_state<=n_state;
+  end
+      
+      //next state logic
+      
+      always@(*)begin
+        n_state=3'b000;
+        case(p_state)
+          3'b000:begin
+            if(in)
+              n_state=3'b001;
+            else
+              n_state=3'b000;
+          end
+          3'b001:begin
+            if(in)
+              n_state=3'b010;
+            else
+              n_state=3'b000;
+          end
+          3'b010:begin
+            if(in)
+              n_state=3'b010;
+            else
+              n_state=3'b011;
+          end
+          3'b011:begin
+            if(in)
+              n_state=3'b100;
+            else
+              n_state=3'b000;
+          end
+          3'b100:begin
+            if(in)
+              n_state=3'b010;
+            else
+              n_state=3'b000;
+          end
+        default:n_state=3'b000;
+          endcase
+      end
+          
+          //output logic
+
+    assign detect=(p_state==3'b100);
+          endmodule
