6efe260e6bc2931e84d6dd20862ad7b52d363c46
arjunsankr <arjunsankarop@gmail.com>
2026-08-01 02:47:13 +0530

    update non-overlapping state detection module

---
+++ rtl/sqdt_1101_mly_nonovr.v
@@ -0,0 +1,63 @@
+module sqdt_1101_nonovr(
+    input clk,
+    input rstn,
+    input in,
+    output detect
+);
+
+reg [1:0] n_state, p_state;
+
+// Current state logic
+
+always @(posedge clk or negedge rstn) begin
+    if(!rstn)
+        p_state <= 2'b00;
+    else
+        p_state <= n_state;
+end
+
+// Next state logic
+
+always @(*) begin
+    n_state = 2'b00;
+
+    case(p_state)
+
+        2'b00: begin
+            if(in == 1'b1)
+                n_state = 2'b01;
+            else
+                n_state = 2'b00;
+        end
+
+        2'b01: begin
+            if(in == 1'b1)
+                n_state = 2'b10;
+            else
+                n_state = 2'b00;
+        end
+
+        2'b10: begin
+            if(in == 1'b0)
+                n_state = 2'b11;
+            else
+                n_state = 2'b10;
+        end
+
+        2'b11: begin
+            if(in == 1'b1)
+                n_state = 2'b00;   // Non-overlapping
+            else
+                n_state = 2'b00;
+        end
+
+        default: n_state = 2'b00;
+
+    endcase
+end
+
+// Output logic
+
+assign detect = (p_state == 2'b11 && in == 1'b1);
+
+endmodule
